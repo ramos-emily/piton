d2e9a6c965de4d5144c64b89928cd118e0a31baa
Emily Vit�ria de Souza Ramos <42749126800@>
2024-11-07 16:15:32 -0300

    first commit

---
+++ morte.py
@@ -0,0 +1,91 @@
+# def calc_media(nota1, nota2):
+#     media = (nota1 + nota2)/2
+#     return media
+
+# nota1 = float(input("digite primeira nota: "))
+# nota2 = float(input("digite segunda nota: "))
+
+# result = calc_media(nota1, nota2)
+# print(result)
+
+# def saudeiction():
+#     print("oi, bem vinde")
+
+# saudeiction()
+
+# def dobre(num):
+#     num = int(input("digite um numero: "))
+#     return num*2
+
+# def media(1,2,3,4,5):
+    
+
+
+
+
+
+
+
+# def IMC():
+#     peso = float(input("digite seu peso: "))
+#     altura = float(input("digite sua altura: "))
+#     result = peso/altura**2
+#     print(result)
+
+# IMC()
+
+# lista = []
+# def maior_n():
+#     quant = int(input("digite a quantidade: "))
+#     for i in range(1,quant+1):
+#         n = int(input(f"digite o numero {i}: "))
+#         lista.append(n)
+#     result = max(lista)
+#     print(result)
+
+# maior_n()
+
+
+# def par_ou_impar():
+#     num = int(input("digite um n inteiro: "))
+#     if num%2 == 0:
+#         print("É par!")
+#     else:
+#         print("É impar!")
+
+# par_ou_impar()
+
+
+# def conta_vogais():
+#     palavra = input("digite uma palavra: ")
+#     result = {} 
+#     vogais = 'aeiou' 
+#     for i in vogais: 
+#         if i in palavra: 
+#             result[i] = palavra.count(i) 
+#     return result 
+ 
+# print(conta_vogais()) 
+
+
+# def contagem_regressiva():
+#     num = int(input("digite um numero: "))
+#     for i in range(num, 0, -1):
+#         print(i)
+
+# contagem_regressiva()
+
+
+
+
+# def conta_letras():
+#     palavra = input("digite uma palavra: ")
+#     result = len(palavra)
+#     for i in palavra:
+#         lista = {i}
+#         print(lista, {result})
+
+# conta_letras()
+
+
+
