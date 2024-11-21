fec99f7dc5159a7cdc29724dd32f2666f6c7076a
Emily Vit�ria de Souza Ramos <42749126800@>
2024-11-21 16:31:10 -0300

    manipulação de arquivo

---
+++ arquivo.py
@@ -0,0 +1,42 @@
+# txt (bloco de notas)
+# with open ("my_arquivo.txt", "w") as arquivo:   # W serve para escrever
+#     arquivo.write("dorDeCabeça\n")
+#     arquivo.write("aaaaaaaaaaa")
+
+# with open("my_arquivo.txt", "r") as arquivo:  # R serve para ler
+#     print(arquivo.read())
+
+
+
+# CSV  (separa as informações por virgula, nao em formato de tabela)
+
+# import csv
+
+# with open ("batatinha.csv", "w", newline='') as csvfile:  #nweline pula linha   
+
+#     seila = csv.writer(csvfile)
+#     seila.writerow(['Nome', 'preco'])
+#     seila.writerow(['livro', '20'])
+
+# arquivo = open('batatinha.csv')
+
+# linhas = csv.reader(arquivo)
+
+# for linha in linhas:
+#     print(linha)
+
+
+
+# JSON (estrutura de arquivo, não é dicionário)
+
+
+
+
+
+# XML (mais verboso, usado em receita federal, antiga)
+
+
+
+
+
+# XLSX (Excel)
---
+++ somativa.py
@@ -0,0 +1,27 @@
+# # 1. Exercício: Crie um algoritmo que receba 10 números e determine quantos deles são pares.
+
+
+# cont = 0
+
+# for i in range(10):
+#     num = int(input("digite um numero: "))
+
+#     if (num % 2) == 0:
+#         print("é par")
+#         cont += 1
+#     else:
+#         print("impar")
+
+# print(f"voce digitou {cont} numeros pares")
+
+
+# 2. Exercício: Crie um algoritmo que leia uma lista de números e retorne o maior número que seja menor que 100.
+
+
+list = int(1, 2, 3, 4, 5, 99, 84)
+cont = 0
+
+for i in range(list):
+    print(list)
+    cont += 1
+
